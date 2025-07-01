c5c2014a496156d9faef333162f3122de8ff5eac
priyaborad-0609 <pp13120@gmail.com>
2025-07-01 08:56:49 +0530

    Completed

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,16 +1,16 @@
 export default function BookCard(props) {
   console.log(props)
   return (
-    <div data-testid="book-card">
+    <div id="book-card" data-testid="book-card">
       <img src={props.img} alt={""} />
       <b>
-        <div data-testid="book-card-title">
+        <div id="book-card-title" data-testid="book-card-title">
           {props.title}
           <span>{props.year}</span>
         </div>
       </b>
-      <div data-testid="book-card-author">{props.author}</div>
-      <div data-testid="book-card-price">{props.price}</div>
+      <div id="book-card-author" data-testid="book-card-author">{props.author}</div>
+      <div id="book-card-price" data-testid="book-card-price">{props.price}</div>
     </div>
   );
 }
